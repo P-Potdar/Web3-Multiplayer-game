c605dddd8cfc53e948a652a5ed6b473953de8310
prianshuamarkhalde <prianshukhalde04@gmail.com>
2024-03-15 17:49:00 +0530

    jsdhfjksdhfdjskfh

--- web3/hardhat.config.ts
+++ web3/hardhat.config.ts
@@ -3,13 +3,7 @@ import '@nomiclabs/hardhat-ethers';
 
 dotenv.config();
 
-//* Notes for deploying the smart contract on your own subnet
-//* More info on subnets: https://docs.avax.network/subnets
-//* Why deploy on a subnet: https://docs.avax.network/subnets/when-to-use-subnet-vs-c-chain
-//* How to deploy on a subnet: https://docs.avax.network/subnets/create-a-local-subnet
-//* Transactions on the C-Chain might take 2-10 seconds -> the ones on the subnet will be much faster
-//* On C-Chain we're relaying on the Avax token to confirm transactions -> on the subnet we can create our own token
-//* You are in complete control over the network and it's inner workings
+
 
 export default {
   solidity: {
@@ -29,11 +23,7 @@ export default {
       chainId: 43113,
       accounts: [process.env.PRIVATE_KEY],
     },
-    // subnet: {
-    //   url: process.env.NODE_URL,
-    //   chainId: Number(process.env.CHAIN_ID),
-    //   gasPrice: 'auto',
-    //   accounts: [process.env.PRIVATE_KEY],
-    // },
+ 
   },
 }
+//pop[opopo]
